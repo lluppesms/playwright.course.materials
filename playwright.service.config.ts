c4a2f439ed0fc3e5cd6703b94ad694c4d6b8b252
Lab Luppes <labs@luppes.com>
2025-09-02 16:36:22 -0500

    Update playwright test config for new PW Workspace

---
+++ playwright.service.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from '@playwright/test';
+import { createAzurePlaywrightConfig, ServiceOS } from '@azure/playwright';
+import { DefaultAzureCredential } from '@azure/identity';
+import config from './playwright.config';
+
+/* Learn more about service configuration at https://aka.ms/pww/docs/config */
+export default defineConfig(
+  config,
+  createAzurePlaywrightConfig(config, {
+    exposeNetwork: '<loopback>',
+    connectTimeout: 3 * 60 * 1000, // 3 minutes
+    os: ServiceOS.LINUX,
+    credential: new DefaultAzureCredential(),
+  })
+);
